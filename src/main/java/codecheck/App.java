3097d08239ce664120dc06fcb696d9f964f20b0f
code-project <dev@code-check.io>
2018-06-06 21:15:19 +0000

    Add new CLI related files

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,13 +5,6 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
-            if(output=="World"){
-                System.out.println("Hello World");
-            }if(output=="織田信長"){
-                System.out.println("Hello 織田信長");
-            }if(output=="codecheck"){
-                System.out.println("Hello codecheck!");
-            }
 		}
 	}
 }
